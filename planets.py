95bce0b387c4580b8242a2b7e087354b43dc0a83
AKoszewski <est@onet.com.pl>
2020-03-14 20:01:52 +0100

    Klasa na symulacje

--- planets.py
+++ planets.py
@@ -2,7 +2,7 @@ import math
 import random
 from easygraphics import *
 
-maxTheta = 1
+maxTheta = 2*math.pi
 
 class Planet:
     def __init__(self, r, theta):
@@ -12,7 +12,7 @@ class Planet:
         self.period = round(math.sqrt(c * r**3)) # from Third Kepler's Law
 
     def calculateTheta(self, time):
-        return (self.theta + time/self.period) % maxTheta
+        return (self.theta + time/self.period*maxTheta) % maxTheta
 
 class System:
     planets = []
@@ -31,32 +31,38 @@ class System:
             print("\ttheta = ", planet.theta)
             print("\tperiod = ", planet.period)
             i = i + 1
-            
-    def draw(self, time, isAnimation):
+
+class Simulation:
+    def __init__(self, system):
+        self.system = system
+
+    def run(self, time, angle, speed, origin):
         rx = 800
         ry = 600
         r = 0.45 * ry
+        isAnimation = True
         init_graph(rx, ry)
         set_render_mode(RenderMode.RENDER_MANUAL)
         set_fill_color(Color.BLACK)
         while is_run():
             if delay_jfps(40):
                 clear_device()
-                self.drawFrame(rx, ry, time)
+                self.__drawFrame(rx, ry, time)
                 if isAnimation:
                     time = time + 1
         close_graph()
 
-    def drawFrame(self, rx, ry, time):
-        for p in self.planets:
-            r = p.r*(0.45*ry/self.planets[-1].r)
+    def __drawFrame(self, rx, ry, time):
+        for p in self.system.planets:
+            r = p.r*(0.45*ry/self.system.planets[-1].r)
             circle(rx/2, ry/2, r)
             theta = p.calculateTheta(time)
-            draw_circle(rx/2 + r*math.cos(theta*2*math.pi), ry/2 + r*math.sin(theta*2*math.pi), 5)
+            draw_circle(rx/2 + r*math.cos(theta), ry/2 + r*math.sin(theta), 5)
 
 system = System(5, 1.618)
-system.print()
-system.draw(0, isAnimation = True)
+simulation = Simulation(system)
+simulation.run(time = 0, angle = 0, speed = 1, origin = 2)
+#system.print()
 # for t in range(80):
-for i in range(len(system.planets)):
-    print(round(system.planets[i].calculateTheta(1556), 3))
+# for i in range(len(system.planets)):
+    # print(round(system.planets[i].calculateTheta(1556), 3))
