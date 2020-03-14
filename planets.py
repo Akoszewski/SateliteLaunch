2dea965a8e89ec6bae2326dcdfd2ba804fc153a3
AKoszewski <est@onet.com.pl>
2020-03-14 17:49:32 +0100

    rysowanie ukladu

--- planets.py
+++ planets.py
@@ -1,5 +1,6 @@
 import math
 import random
+from easygraphics import *
 
 maxTheta = 1
 
@@ -30,10 +31,26 @@ class System:
             print("\ttheta = ", planet.theta)
             print("\tperiod = ", planet.period)
             i = i + 1
-
+            
+    def draw(self):
+        rx = 800
+        ry = 600
+        r = 0.45 * ry
+        # self.planets[-1].r
+        # set_fill_color(Color.LIGHT_BLUE)
+        init_graph(rx, ry)
+        set_fill_color(Color.BLACK)
+        for p in self.planets:
+            r = p.r*(0.45*ry/self.planets[-1].r)
+            circle(rx/2, ry/2, r)
+            draw_circle(rx/2 + r*math.cos(p.theta*2*math.pi), ry/2 + r*math.sin(p.theta*2*math.pi), 5)
+        while is_run():
+            x = 1
+        close_graph()
 
 system = System(5, 1.618)
 system.print()
-# for t in range(80):
+system.draw()
+# for t in range(80):s
 for i in range(len(system.planets)):
     print(round(system.planets[i].calculateTheta(1556), 3))
